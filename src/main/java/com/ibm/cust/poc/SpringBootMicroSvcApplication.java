ba6413640e0594b26ffdfb22232ed85fd21327ac
GIRISH MEHER <girmeher@in.ibm.com>
2018-02-27 13:58:47 +0530

    initial version

---
+++ src/main/java/com/ibm/cust/poc/SpringBootMicroSvcApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.cust.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroSvcApplication.class, args);
+	}
+}
